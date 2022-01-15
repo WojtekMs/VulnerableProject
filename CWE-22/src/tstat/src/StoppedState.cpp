85078847204ff2d350c3cf940fa65ba57bc45bdc
WojtekMs <wojtek.atmo@gmail.com>
2022-01-15 17:15:57 +0100

    Make CWE-22 more insecure
    - change the way it is working a little bit, to make the error more effective

--- CWE-22/src/tstat/src/StoppedState.cpp
+++ CWE-22/src/tstat/src/StoppedState.cpp
@@ -23,16 +23,12 @@ void StoppedState::stop()
 void StoppedState::save()
 {
     auto file_path = getFilePath();
-    auto task_directory = utils::join('/', config::log_save_path, counter_.task_name_);
-    if (!std::filesystem::exists(task_directory)) {
-        std::filesystem::create_directories(task_directory);
-    }
     std::ios_base::openmode file_flags{};
     if (std::filesystem::exists(file_path)) {
         file_flags = std::ios_base::out | std::ios_base::app;
     }
     std::ofstream output_file(file_path, file_flags);
-    output_file << getElapsedTime() << '\n';
+    output_file << utils::getCurrentDate() << ";" << getElapsedTime() << '\n';
     counter_.reset();
     counter_.counter_state_ = std::make_unique<IdleState>(counter_);
 }
@@ -48,7 +44,7 @@ std::string StoppedState::getElapsedTime() const {
 }
 
 std::string StoppedState::getFilePath() const {
-    return utils::join('/', config::log_save_path, counter_.task_name_, utils::getCurrentDate());
+    return utils::join('/', config::log_save_path, counter_.task_name_);
 }
 
 std::string StoppedState::getCurrentStateInfo() const noexcept {
